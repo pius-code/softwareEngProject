4f47a1160b322000130d125a67e8f5aa1800b19b
pius-code <56304861+pius-code@users.noreply.github.com>
2024-07-25 07:30:02 +0000

    Delete cpen200 directory

--- cpen200/src/main/java/eng/cpen/cpen200/TARepo/TARepo.java
+++
@@ -1,9 +0,0 @@
-package eng.cpen.cpen200.TARepo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import eng.cpen.cpen200.TAModel.TAModel;
-
-public interface TARepo extends JpaRepository<TAModel, Long> {
-
-}
--- cpen200/src/main/java/eng/cpen/cpen200/TeaRepo/TRepo.java
+++
@@ -1,9 +0,0 @@
-package eng.cpen.cpen200.TeaRepo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import eng.cpen.cpen200.TeaModel.Tmodel;
-
-public interface TRepo extends JpaRepository<Tmodel, Long> {
-
-}
